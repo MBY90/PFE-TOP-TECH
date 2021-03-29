7c22dec2f4ad3ea816f9ac3f58919ba8f1a2ce22
mby90 <med.ben.youssefisi@gmail.com>
2021-03-29 14:02:05 +0100

    first commit

---
+++ config/connectDB.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const config = require("config");
+
+const connectDB = () => {
+  mongoose
+    .connect(config.get("MONGOURI"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => console.log("mongoose is connected "))
+    .catch(() => console.log("Error"));
+};
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,19 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const connectDB = require("./config/connectDB");
+
+
+connectDB();
+
+app.use(express.json());
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, (err) =>
+  err
+    ? console.log("Error", err)
+    : console.log(`the server is running on port ${port}`)
+);
